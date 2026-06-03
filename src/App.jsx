6b876f554259b8f93b227a20913c0e0ebe8ff1ac
Kanha Tyagi <kanhatyagi2005@gmail.com>
2026-06-03 13:26:59 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1> Expense tracker</h1>
+        <p>studio graphene assignment </p>
+    </div>
+  )
+}
+
+export default App
